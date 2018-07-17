0d0fa73ac9b7518d08fe78871088f31f45033997
Aliz Karami <aliz@mie.utoronto.ca>
2018-07-17 10:37:11 -0400

    Show hide

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// show / hide function
+function toggleShowByClass(className, displayState){
+    const elements = document.getElementsByClassName(className);
+
+    for (var i = 0; i < elements.length; i++){
+        elements[i].style.display = displayState;
+    }
+}
+
+function displaySection(id){
+    const element = document.getElementById(id);
+    element.style.display = 'block';
+}
+
+// click handler
+document.addEventListener('click', function(e) {
+    e = e || window.event;
+    //target that has been clicked
+    const target = e.target || e.srcElement;
+
+    //get class name
+    let this_class = target.className;
+
+
+    // toggle show and hide each section
+    if (this_class === 'degree-program') {
+    	const clicke_id = target.id;
+    	toggleShowByClass('program-desc', 'none');
+    	if (clicke_id === 'g') {
+    		displaySection('grad');
+    	} else if (clicke_id === 'ug') {
+			displaySection('u-grad');
+    	} else {
+			displaySection('non-degree');
+    	}
+    }
+    
+}, false);
